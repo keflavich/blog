62ac0eaab218caabc1de5745746b52d585854c4d
Adam Ginsburg <keflavich@gmail.com>
2013-02-27 19:33:25 -0700

    theme!  changed to... whatever

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,20 +3,24 @@
 
 AUTHOR = u'Adam Ginsburg'
 SITENAME = u"Adam Ginsburg's blog"
-SITEURL = ''
+SITEURL = 'http://keflavich.github.com/blog/'
 
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
 
 # Blogroll
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
+#LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+#          ('Python.org', 'http://python.org'),
+#          ('Jinja2', 'http://jinja.pocoo.org'),
+#          ('You can modify those links in your config file', '#'),)
+LINKS=()
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+SOCIAL=()
 
 DEFAULT_PAGINATION = 10
+
+THEME='mnmlist'
